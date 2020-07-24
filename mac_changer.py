0171963dacf67c60da06411ebc962090a92a61c6
kbrooks87 <kbrooks10011987@gmail.com>
2020-07-24 06:49:32 -0400

    current status is working but other then linux is unknow at this time

---
+++ mac_changer.py
@@ -0,0 +1,11 @@
+import subprocess
+
+interface = input("interface >")
+new_mac = input("new Mac address >")
+
+print("[+] Change MAC address for " + interface + " to " + new_mac)
+
+subprocess.call("ifconfig " + interface + "down", shell=True)
+subprocess.call("ifconfig " + interface + " hw ether " + new_mac, shell=True)
+subprocess.call("ifconfig " + interface + "up", shell=True)
+subprocess.call("ifconfig", shell=True)
