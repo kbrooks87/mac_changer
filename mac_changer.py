39b21c6d30cb9b1855de90fd72e0119aad8e6f44
kbrooks87 <kbrooks10011987@gmail.com>
2020-07-25 01:00:56 -0400

    turned the way to change the MAC address into a function and removed unneeded code

--- mac_changer.py
+++ mac_changer.py
@@ -1,6 +1,14 @@
 import subprocess
 import optparse
 
+def change_mac(interface, new_mac):
+    print("[+] Change MAC address for " + interface + " to " + new_mac)
+
+    subprocess.call(["ifconfig", interface, "down"])
+    subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
+    subprocess.call(["ifconfig", interface, "up"])
+    subprocess.call(["ifconfig"])
+
 parser = optparse.OptionParser()
 
 parser.add_option("-i", "--interface", dest="interface", help="Interface to change MAC address")
@@ -8,12 +16,4 @@ parser.add_option("-m", "--mac", dest="new_mac", help="the new MAC address")
 
 (options, arguments) = parser.parse_args()
 
-interface = options.interface
-new_mac = options.new_mac
-
-print("[+] Change MAC address for " + interface + " to " + new_mac)
-
-subprocess.call(["ifconfig", interface, "down"])
-subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
-subprocess.call(["ifconfig", interface, "up"])
-subprocess.call(["ifconfig"])
+change_mac(options.interface, options.new_mac)
