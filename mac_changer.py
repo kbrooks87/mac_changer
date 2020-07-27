6af4ca2bbc11da41f074d8eac2e60b38679b32bc
kbrooks87 <kbrooks10011987@gmail.com>
2020-07-27 00:11:14 -0400

    added error responses

--- mac_changer.py
+++ mac_changer.py
@@ -1,6 +1,18 @@
 import subprocess
 import optparse
 
+
+def get_arguments():
+    parser = optparse.OptionParser()
+    parser.add_option("-i", "--interface", dest="interface", help="Interface to change MAC address")
+    parser.add_option("-m", "--mac", dest="new_mac", help="the new MAC address")
+    (options, arguments) = parser.parse_args()
+    if not options.interface:
+        parser.error("[-] Please specify an interface. use --help for more info.")
+    elif not options.new_mac:
+        parser.error("[-] please specify a new MAC address. use --help for more info.")
+    return options
+
 def change_mac(interface, new_mac):
     print("[+] Change MAC address for " + interface + " to " + new_mac)
 
@@ -14,6 +26,5 @@ parser = optparse.OptionParser()
 parser.add_option("-i", "--interface", dest="interface", help="Interface to change MAC address")
 parser.add_option("-m", "--mac", dest="new_mac", help="the new MAC address")
 
-(options, arguments) = parser.parse_args()
-
+options = get_arguments()
 change_mac(options.interface, options.new_mac)
